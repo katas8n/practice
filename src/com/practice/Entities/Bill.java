b7a109c02d32f248d016b846f3134049912ee6dc
katas8n1 <kirka6061512@icloud.com>
2024-06-26 21:03:06 +0200

    Commit kek

---
+++ src/com/practice/Entities/Bill.java
@@ -0,0 +1,9 @@
+package com.practice.Entities;
+
+public class Bill {
+//    money
+//    ID
+//    Name Surname
+
+    //WALLET AND CARD
+}
---
+++ src/com/practice/Entities/Card.java
@@ -0,0 +1,10 @@
+package com.practice.Entities;
+
+public class Card {
+// id
+// cv
+// money
+// date(STRING) in format mm/DD/yyyy
+
+// isValid
+}
