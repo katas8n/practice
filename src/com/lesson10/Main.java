63363d16ba63ba950f4c5b4ced077fcecf7825d0
Andriy Ivanchenko <clawhammer91@gmail.com>
2024-07-17 18:10:13 +0200

    feat: added toDoList

--- src/com/lesson10/Main.java
+++ src/com/lesson10/Main.java
@@ -41,30 +41,39 @@ public class Main {
 //                Вывод всех задач, выполненных задач и невыполненных задач на экран.
 
         Scanner scanner = new Scanner(System.in);
-        String[] defaultChars = {"a", "b", "c", "d"};
-        String[] mainMenu = {"Add task in ToDoTask", "Mark Task as not pending", "Show all tasks", "Quit"};
+        String[] defaultChars = {"a", "b", "c", "d", "e", "f", "g"};
+        String[] mainMenu = {"Add task", "Show all tasks", "Show completed tasks", "Complete task", "Update task", "Delete task", "Exit"};
         HashMap<String, String> Menu = new HashMap<>();
         Utils.fillMenuItems(mainMenu, defaultChars, Menu);
         ToDoList toDoList = new ToDoList();
         boolean isRunning = true;
+        int id = 1;
 
         while (isRunning) {
             System.out.println("What would u like to do?");
             Utils.showMenuItems(Menu);
             String answer = scanner.nextLine().toLowerCase();
-            int id = 1;
             switch (answer){
                 case "a":
                     addTask(toDoList, id);
                     id++;
                     break;
                 case "b":
-                    markTaskDone(toDoList);
-                    break;
-                case "c":
                     showAllTasks(toDoList);
                     break;
+                case "c":
+                    showCompletedTasks(toDoList);
+                    break;
                 case "d":
+                    markTaskDone(toDoList);
+                    break;
+                case "e":
+                    updateTask(toDoList);
+                    break;
+                case "f":
+                    deleteTask(toDoList);
+                    break;
+                case "g":
                     isRunning = false;
                     break;
                 default:
@@ -128,5 +137,84 @@ public class Main {
         toDoList.getPendingTasks();
         System.out.println("Ur completed tasks:");
         toDoList.getCompletedTasks();
+    }
+
+    public static void showCompletedTasks(ToDoList toDoList) {
+        System.out.println("Ur completed tasks:");
+        toDoList.getCompletedTasks();
+    }
+
+    public static void updateTask(ToDoList toDoList) {
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+        toDoList.getAllTasks();
+        System.out.println("Choose the task which you want to update. Type the task ID:");
+        int id = scanner.nextInt();
+
+        Task task = toDoList.findTaskById(id);
+        if (task != null) {
+            System.out.println("Enter your title of task");
+            String title = scanner.nextLine();
+            System.out.println("Enter your description of task");
+            String description = scanner.nextLine();
+
+            LocalDateTime deadline = null;
+            boolean isValidDeadLine = false;
+
+            while (!isValidDeadLine) {
+                System.out.println("Enter your deadline of task\nWrite in format: yyyy-MM-dd HH:mm");
+                String dateTime = scanner.nextLine();
+
+                try {
+                    deadline = LocalDateTime.parse(dateTime, formatter);
+                    if (deadline.isAfter(LocalDateTime.now())) {
+                        isValidDeadLine = true;
+                    } else {
+                        System.out.println("Your deadline is in the past. Please enter a future date and time.");
+                    }
+                } catch (DateTimeParseException e) {
+                    System.out.println("Invalid format. Please use the format: yyyy-MM-dd HH:mm");
+                }
+            }
+            System.out.println("Is ur task completed? Write y or n");
+            String answer = scanner.nextLine();
+            boolean isValidAnswer = false;
+            while (!isValidAnswer) {
+                if (answer.equalsIgnoreCase("y")) {
+                    task.setDone(true);
+                    isValidAnswer = true;
+                } else if (answer.equalsIgnoreCase("n")) {
+                    task.setDone(false);
+                    isValidAnswer = true;
+                } else {
+                    System.out.println("Incorrect answer\nTry again!");
+                }
+            }
+
+            task.setTitle(title);
+            task.setDescription(description);
+            task.setDeadline(deadline);
+            task.setId(id);
+            System.out.println("Ur currently tasksList looks so:");
+            toDoList.getAllTasks();
+        } else {
+            System.out.println("Task with ID " + id + " not found.");
+        }
+    }
+
+    public static void deleteTask(ToDoList toDoList) {
+        Scanner scanner = new Scanner(System.in);
+        toDoList.getAllTasks();
+        System.out.println("Choose the task which you want to remove. Type the task ID:");
+        int id = scanner.nextInt();
+
+        Task task = toDoList.findTaskById(id);
+        if (task != null) {
+            toDoList.removeTask(task);
+            System.out.println("Task is removed.");
+        } else {
+            System.out.println("Task with ID " + id + " not found.");
+        }
     }
 }
